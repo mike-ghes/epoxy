145b8c82daaac7e42faf5fa456346d5f1b9b6e3e
Mike Ghesquiere <>
2022-02-01 18:25:02 -0800

    Shuffle items triggers crash

--- kotlinsample/src/main/java/com/airbnb/epoxy/kotlinsample/StickyHeaderController.kt
+++ kotlinsample/src/main/java/com/airbnb/epoxy/kotlinsample/StickyHeaderController.kt
@@ -11,23 +11,49 @@ import com.airbnb.epoxy.stickyheader.StickyHeaderCallbacks
 /**
  * Showcases [EpoxyController] with sticky header support
  */
+
+data class Item(val id: Int) {
+    val isSticky = (id % 5 == 0)
+}
+
 class StickyHeaderController(
     private val context: Context
 ) : EpoxyController(), StickyHeaderCallbacks {
 
-    var items = (0 until 100).map { Pair(it, (it % 5) == 0) }
+    var items = (0 until 100).map { Item(it) }
         set(value) {
             field = value
             requestModelBuild()
         }
 
+    fun reset() {
+        items = (0 until 100).map { Item(it) }
+    }
 
     override fun buildModels() {
+
+        itemEpoxyHolder {
+            id("reset")
+            title("Reset")
+            listener {
+                this@StickyHeaderController.reset()
+            }
+        }
+
+
+        itemEpoxyHolder {
+            id("shuffle-items")
+            title("Shuffle")
+            listener {
+                this@StickyHeaderController.items = this@StickyHeaderController.items.shuffled()
+            }
+        }
+
         items.forEach {
-            if (it.second) {
+            if (it.isSticky) {
                 stickyItemEpoxyHolder {
-                    id("sticky-header ${it.first}")
-                    title("Sticky header ${it.first}")
+                    id("sticky-header ${it.id}")
+                    title("Sticky header ${it.id}")
                     listener {
                         this@StickyHeaderController.items -= it
                         Toast.makeText(
@@ -37,9 +63,10 @@ class StickyHeaderController(
                         ).show()
                     }
                 }
-            } else { itemEpoxyHolder {
-                    id("view holder ${it.first}")
-                    title("this is a View Holder item ${it.first}")
+            } else {
+                itemEpoxyHolder {
+                    id("view holder ${it.id}")
+                    title("this is a View Holder item ${it.id}")
                     listener {
                         this@StickyHeaderController.items -= it
                         Toast.makeText(
@@ -54,5 +81,6 @@ class StickyHeaderController(
     }
 
     // Feel feel to use any logic here to determine if the [position] is sticky view or not
-    override fun isStickyHeader(position: Int) = adapter.getModelAtPosition(position) is StickyItemEpoxyHolder
+    override fun isStickyHeader(position: Int) =
+        adapter.getModelAtPosition(position) is StickyItemEpoxyHolder
 }
